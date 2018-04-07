4f1c2f9365022c54616f261290d0a67b46e98931
yusukemisa <yusukemisawa2014@gmail.com>
2018-04-07 12:52:54 +0900

    ・テスト用ファイル追加 ・引数処理機能追加 ・ファイル内容表示処理追加

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("ほげええ")
+	//引数処理
+	n := flag.Int("n", 10, "出力行数")
+	flag.Parse()
+
+	fileName := flag.Arg(0)
+	//fmt.Printf("n=%v,fileName=%v\n", *n, fileName)
+
+	//ファイル読み込み
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatalln(fileName + "が開けませんでした")
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	for i := 0; i < *n; i++ {
+		line, err := reader.ReadString('\n')
+		fmt.Print(line)
+		if err == io.EOF {
+			break
+		}
+	}
 }
