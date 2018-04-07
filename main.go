78dac4c4832e284b0436370a078d12b2eaad5e99
yusukemisa <yusukemisawa2014@gmail.com>
2018-04-07 19:02:05 +0900

    ・出力用ファイル追加 ・cオプション機能実装 ・複数ファイル指定機能実装

--- main.go
+++ main.go
@@ -5,31 +5,65 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"log"
 	"os"
 )
 
 func main() {
 	//引数処理
 	n := flag.Int("n", 10, "出力行数")
+	c := flag.Int64("c", 0, "出力バイト数")
 	flag.Parse()
-
-	fileName := flag.Arg(0)
-	//fmt.Printf("n=%v,fileName=%v\n", *n, fileName)
+	// n c同時オプション指定禁止
+	if flag.NFlag() != 1 {
+		fmt.Fprintf(os.Stderr, "%v: can't combine line and byte counts\n", os.Args[0])
+		flag.Usage()
+		return
+	}
+	files := flag.Args()
+	//fmt.Printf("len=%v,files=%v", len(files), files)
 
 	//ファイル読み込み
-	file, err := os.Open(fileName)
-	if err != nil {
-		log.Fatalln(fileName + "が開けませんでした")
-	}
-	defer file.Close()
+	readFiles(files, *n, *c)
+}
 
-	reader := bufio.NewReader(file)
-	for i := 0; i < *n; i++ {
-		line, err := reader.ReadString('\n')
-		fmt.Print(line)
-		if err == io.EOF {
-			break
+func readFiles(files []string, n int, c int64) {
+	for i, fileName := range files {
+		file, err := os.Open(fileName)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v: No such file or directory\n", fileName)
+			file.Close()
+			continue
+		}
+		//複数ファイル指定時のヘッダー出力
+		printHeader(i, len(files), fileName)
+
+		readFile(bufio.NewReader(file), n, c)
+		//forの全ループがスコープになるのでdeferで閉じるのよくないみたいなので手動で都度クローズ
+		file.Close()
+	}
+}
+
+func printHeader(i int, length int, fileName string) {
+	if 0 < i {
+		fmt.Println("")
+	}
+	if 1 < length {
+		fmt.Printf("==> %v <==\n", fileName)
+	}
+}
+
+func readFile(reader *bufio.Reader, n int, c int64) {
+	if 0 < c {
+		//バイト数指定出力
+		io.CopyN(os.Stdout, reader, c)
+	} else {
+		//行数指定出力
+		for i := 0; i < n; i++ {
+			line, err := reader.ReadString('\n')
+			fmt.Print(line)
+			if err == io.EOF {
+				break
+			}
 		}
 	}
 }
